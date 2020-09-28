20323819c98414b9bf38da30084de9f0a225100e
bineetNaidu <bineet.s.naidu@gmail.com>
2020-09-28 16:05:55 +0530

    add base title Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Title from './Title';
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets Build a Firegram.</h1>
+      <Title />
     </div>
   );
 }
